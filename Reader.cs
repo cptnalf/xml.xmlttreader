17af1732c8684dddd8831f41fa86ebafeb0c3352
cptnalf <captainalf@gmail.com>
2017-03-07 18:06:41 -0500

    progress xml export reader.
    * read progress (4gl) xml temp-table dumps created with
      tablehandle:write-xml('file', ...)

---
+++ Reader.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xmlttreader
+{
+  /* open file.
+   * look for ttRow
+   *  all the other elements in here are fields.
+   *  find a good mapping?
+   * 
+   */
+  public static class ReaderT
+  {
+    public static Reader<T> Build<T>(FieldMapping.FieldMapper<T> mapper) where T : class, new()
+    {
+      var nr = new Reader<T>();
+      nr.mapper = mapper;
+      return nr;
+    }
+  }
+
+
+  public class Reader<T> : IEnumerable<T> where T : class, new()
+  {
+    private System.Xml.XmlReader _rdr;
+    private T _obj;
+
+    public FieldMapping.FieldMapper<T> mapper {get;set; }
+
+    public void init(string file)
+    {
+      _rdr = System.Xml.XmlReader.Create(new System.IO.FileStream(file, System.IO.FileMode.Open, System.IO.FileAccess.Read, System.IO.FileShare.ReadWrite));
+
+      _rdr.MoveToContent();
+      _rdr.Read();
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+      T obj = null;
+
+      while(!_rdr.EOF && _rdr.ReadState == System.Xml.ReadState.Interactive)
+        {
+          if (_rdr.NodeType == System.Xml.XmlNodeType.Element)
+            {
+              if (_rdr.Name == "tt") {  }
+              else
+               {
+                  if (_rdr.Name == "ttRow")
+                    {
+                      if (obj != null) { yield return obj; }
+                      obj = mapper.create();
+                    }
+                  else
+                    {
+                      if (obj != null)
+                        {
+                          var name = _rdr.Name;
+                          var contents = _rdr.ReadElementContentAsString();
+              
+                          mapper.set(obj, name, contents);
+                        }
+                    }
+                }
+            }
+
+          _rdr.Read();
+        }
+
+      if (obj != null) { yield return obj; }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() { return this.GetEnumerator(); }
+  }
+}
---
+++ TTReaderTest/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TTReaderTest
+{
+  public class RTBPMod
+  {
+    public string pmod {get;set; }
+    public int nextObjNum {get;set; }
+    public string desc { get;set; }
+    public string prodID {get;set; }
+    public string module {get;set; }
+    public int verCounter { get;set; }
+  }
+
+  [TestClass]
+  public class UnitTest1
+  {
+    [TestMethod]
+    public void TTReader()
+    {
+      var rdr = xmlttreader.ReaderT.Build(FieldMapping.FieldMapper<RTBPMod>.Create()
+        .field(x => x.pmod, "pmod")
+        .field(x => x.nextObjNum, "nxt-obj-num")
+        .field(x => x.desc, "description")
+        .field(x => x.prodID, "product-id")
+        .field(x => x.module, "Module")
+        .field(x => x.verCounter, "ver-counter")
+        );
+
+      rdr.init("c:\\tmp\\rtb.rtb_pmod.xml");
+
+      var lst = rdr.ToList();
+      Assert.IsNotNull(lst);
+      Assert.IsTrue(lst.Count > 0);
+    }
+  }
+}
