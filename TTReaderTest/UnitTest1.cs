dd6f8ac6e6e977b61bcf51389822d76cb3687387
cptnalf <captainalf@gmail.com>
2017-03-27 17:57:35 -0400

    add more unit tests.

--- TTReaderTest/UnitTest1.cs
+++ TTReaderTest/UnitTest1.cs
@@ -15,6 +15,16 @@ namespace TTReaderTest
     public int verCounter { get;set; }
   }
 
+  public class Task1
+  {
+    public int id {get;set;}
+    public List<string> description {get;set;}
+    public string summary {get;set;}
+    public DateTime completed {get;set;}
+
+    public Task1() { this.description = new List<string>(); }
+  }
+
   [TestClass]
   public class UnitTest1
   {
@@ -30,11 +40,40 @@ namespace TTReaderTest
         .field(x => x.verCounter, "ver-counter")
         ;
 
+      /* construct test to test all values.
+       * int, string, long, ulong, uint, bool, list,
+       * objects, datetime, nullable<*>
+       */
       var rdr = xmlttreader.ReaderT.Build(mapper, "c:\\tmp\\rtb.rtb_pmod.xml");
 
       var lst = rdr.ToList();
       Assert.IsNotNull(lst);
       Assert.IsTrue(lst.Count > 0);
+    }
+
+    [TestMethod]
+    public void TTReader_ArrayTest()
+    {
+      var mapper = FieldMapping.FieldMapper<Task1>.Create(false)
+        .field(x => x.id, "task-num")
+        .field(x => x.summary, "summary")
+        .list(x => x.description,  true, (r,k) => string.IsNullOrWhiteSpace(k) ? null : k, "description")
+        .field(x => x.completed, "entered-when")
+        ;
+
+      var rdr = xmlttreader.ReaderT.Build(mapper, "c:\\tmp\\rtb.rtb_task.xml");
+
+      var lst = rdr.ToList();
+
+      bool haveCompletedNotMin = false;
+      CollectionAssert.AllItemsAreNotNull(lst);
+      foreach(var x in lst)
+        {
+          Assert.AreNotEqual(0, x.id);
+          Assert.AreNotEqual(DateTime.MinValue, x.completed);
+          CollectionAssert.AllItemsAreNotNull(x.description);
+          Assert.IsNotNull(x.summary);
+        }
     }
   }
 }
